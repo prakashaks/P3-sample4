7e9e5a44f5c0acf82674ebf58b534f4c3cefe19b
Prakash Kandasamy <prakashaks97@gmail.com>
2021-02-03 17:11:55 +0530

    Add files via upload

---
+++ src/app/monthly/monthly.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-monthly',
+  templateUrl: './monthly.component.html',
+  styleUrls: ['./monthly.component.css']
+})
+export class MonthlyComponent implements OnInit {
+
+  endDate = new Date();
+  startDate = new Date();
+  defaultOptions = 'End Date';
+  selectedSelect = '';
+  occurrence = 1;
+  date : Date[]= [];
+  frequency = 1;
+  select = [ 'End Date', 'Occurrence','On Going',];
+  months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+  DateFilter = (d: Date | null): boolean =>{
+    const date = (d || new Date());
+    return date >= this.startDate;
+  }
+
+ constructor() { this.endDate.setDate(this.startDate.getDate());
+ }
+
+ ngOnInit(): void {
+ }
+
+setDefault(): void{
+ this.endDate =new Date();
+ if (this.startDate != null){
+   this.endDate.setDate(this.startDate.getDate());
+ }
+ this.occurrence = 1 
+}
+
+SelectDates(): void{
+ this.selectedSelect = this.defaultOptions;
+ this.date = [];
+ let status = true;
+ let tempOccurrance = 0;
+ if(this.selectedSelect === 'End Date') {
+   const end = new Date (this.endDate);
+   do{
+     const tempDate = new Date (this.startDate);
+     tempDate.setDate(tempDate.getDate() + (this.frequency * tempOccurrance));
+   status = tempDate <= end;
+   if(status){
+
+     this.date.push(tempDate);
+   }
+   tempOccurrance++;
+   } while (status);
+  } else if(this.selectedSelect === 'Occurrence'){
+     do{
+      const tempDate = new Date(this.startDate);
+      tempDate.setDate(tempDate.getDate() + (this.frequency * tempOccurrance));
+      this.date.push(tempDate);
+      tempOccurrance++;
+     } while(tempOccurrance !== this.occurrence);
+   }
+ }
+
+  
+}
